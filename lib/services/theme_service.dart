23bc4aabb536f454a469c4a64c49bf404290274c
meriamdref2000 <drefmeriam@gmail.com>
2022-05-11 23:30:50 +0100

    dark and light mode

---
+++ lib/services/theme_service.dart
@@ -0,0 +1,11 @@
+import 'package:get_storage/get_storage.dart';
+
+class ThemeService {
+
+  final _box = GetStorage();
+  final _key = 'isDarkMode';
+
+  bool _loadThemeFromBox() => _box.read(_key)??false;
+  
+  
+}
