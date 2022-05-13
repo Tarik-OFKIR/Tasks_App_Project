43d2620e85b7d7e701e77eaf960643f4e492e81a
meriamdref2000 <drefmeriam@gmail.com>
2022-05-13 01:12:33 +0100

    updating add task page

--- lib/ui/add_task_bar.dart
+++ lib/ui/add_task_bar.dart
@@ -1,11 +1,37 @@
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:tasks_projet_innovation_front/ui/theme.dart';
+import 'package:tasks_projet_innovation_front/ui/widgets/input_field.dart';
 
-class AddTaskPage extends StatelessWidget {
+class AddTaskPage extends StatefulWidget {
   const AddTaskPage({Key? key}) : super(key: key);
 
+  @override
+  State<AddTaskPage> createState() => _AddTaskPageState();
+}
+
+class _AddTaskPageState extends State<AddTaskPage> {
+  DateTime _selectedDate = DateTime.now();
+  String _endTime="9:30 PM";
+  String _startTime = DateFormat("hh:mm a").format(DateTime.now()).toString();
+  int _selectedRemind = 5;
+  List<int> remindList=[
+    5,
+    10,
+    15,
+    20,
+  ];
+  String _selectedRepeat = "None";
+  List<String> repeatList=[
+    "None",
+    "Daily",
+    "weakly",
+    "Monthly"
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,12 +46,145 @@ class AddTaskPage extends StatelessWidget {
                 "Add Task",
                 style: headingStyle,
               ),
+              MyInputField(title: "Title", hint: "Enter your title"),
+              MyInputField(title: "Note", hint: "Enter your note"),
+              MyInputField(title: "Date", hint: DateFormat.yMd().format(_selectedDate),
+              widget: IconButton(
+                icon: Icon(Icons.calendar_today_outlined,
+                color: Colors.grey,
+                ),
+                onPressed: () {
+                  print("Hi there");
+                  _getDateFromUser();
+                },
+
+              ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: MyInputField(
+                    title: "Start Date",
+                    hint: _startTime,
+                        widget: IconButton(
+                          onPressed: () {
+                            _getTimeFromUser(isStartTime: true);
+                          },
+                          icon: Icon(
+                            Icons.access_time_rounded,
+                            color:Colors.grey,
+                          ),
+
+                        ),
+                  )),
+                  SizedBox(width: 12,),
+                  Expanded(
+                      child: MyInputField(
+                        title: "End Date",
+                        hint: _endTime,
+                        widget: IconButton(
+                          onPressed: () {
+                            _getTimeFromUser(isStartTime: false);
+                          },
+                          icon: Icon(
+                            Icons.access_time_rounded,
+                            color:Colors.grey,
+                          ),
+
+                        ),
+                      )),
+                ],
+              ),
+              MyInputField(title: "Remind", hint: "$_selectedRemind minutes early",
+              widget:DropdownButton(
+                icon: Icon(Icons.keyboard_arrow_down,
+                color: Colors.grey,
+              ),
+              iconSize: 32,
+              elevation: 4,
+              style: subTitleStyle ,
+              underline: Container(height: 0,),
+
+              items: remindList.map<DropdownMenuItem<String>>((int value){
+                return DropdownMenuItem<String>(
+                    value: value.toString(),
+                    child: Text(value.toString()),
+                );
+                }
+              ).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _selectedRemind = int.parse(newValue!);
+                  });
+              },
+
+
+              )
+
+
+              ),
+              MyInputField(title: "Repeat", hint: "$_selectedRepeat ",
+                  widget:DropdownButton(
+                    icon: Icon(Icons.keyboard_arrow_down,
+                      color: Colors.grey,
+                    ),
+                    iconSize: 32,
+                    elevation: 4,
+                    style: subTitleStyle ,
+                    underline: Container(height: 0,),
+
+                    items: repeatList.map<DropdownMenuItem<String>>((String? value){
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value!, style: TextStyle(color:Colors.grey)),
+                      );
+                    }
+                    ).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedRepeat = newValue!;
+                      });
+                    },
+
+
+                  )
+
+
+              ),
+              SizedBox(height: 18,)
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Color",
+                      style: titleStyle,
+                      ),
+                      SizedBox(height: 8.0,),
+                      Wrap(
+                        children: List<Widget>.generate(
+                            3,
+                                (int index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.only(right: 8.0),
+                                    child: CircleAvatar(
+                                       radius: 14,
+                                      backgroundColor: index==0?primaryClr:inde,
+                                    ),
+                                  );
+                                }),
+                      )
+                    ],
+                  )
+                ],
+              )
             ],
           ),
         ),
       )
     );
   }
+
   _appBar(BuildContext context){
     return AppBar(
       elevation: 0,
@@ -51,4 +210,47 @@ class AddTaskPage extends StatelessWidget {
     );
   }
 
+  _getDateFromUser() async{
+    DateTime? _pickerDate= await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2014),
+        lastDate: DateTime(2122)
+    );
+    if(_pickerDate!=null){
+      setState(() {
+        _selectedDate = _pickerDate;
+        print(_selectedDate);
+      });
+    }else{
+      print("it's null or something is wrong");
+    }
+  }
+  _getTimeFromUser({required bool isStartTime}) async{
+    var pickedTime = await _showTimePicked();
+    String _formatedTime = pickedTime.format(context);
+    if(pickedTime==null){
+      print("Time canceled");
+    }else if(isStartTime==true){
+      setState(() {
+        _startTime=_formatedTime;
+      });
+    }else if(isStartTime==false){
+      setState(() {
+        _endTime=_formatedTime;
+      });
+
+    }
+
+  }
+  _showTimePicked(){
+    return showTimePicker(
+      initialEntryMode: TimePickerEntryMode.input,
+        context: context,
+        initialTime: TimeOfDay(
+            hour: int.parse(_startTime.split(":")[0]),
+            minute: int.parse(_startTime.split(":")[1].split(" ")[0])
+        )
+    );
+  }
 }
--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -31,7 +31,8 @@ TextStyle get subHeadingStyle{
     textStyle: TextStyle(
       fontSize: 24,
       fontWeight: FontWeight.bold,
-      color: Get.isDarkMode?Colors.white:Colors.black
+        color: Get.isDarkMode?Colors.grey[400]:Colors.grey
+
     )
   );
 }
@@ -40,7 +41,25 @@ TextStyle get headingStyle{
       textStyle: TextStyle(
           fontSize: 30,
           fontWeight: FontWeight.bold,
-          color: Get.isDarkMode?Colors.grey[400]:Colors.grey
+          color: Get.isDarkMode?Colors.white:Colors.black
+      )
+  );
+}
+TextStyle get titleStyle{
+  return GoogleFonts.lato (
+      textStyle: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+          color: Get.isDarkMode?Colors.white:Colors.black
+      )
+  );
+}
+TextStyle get subTitleStyle{
+  return GoogleFonts.lato (
+      textStyle: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+          color: Get.isDarkMode? Colors.grey[100]:Colors.grey[600]
       )
   );
 }
